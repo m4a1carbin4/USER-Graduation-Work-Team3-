f54219c1b560260851989f56bc30f9118b880b32
WGNW_gangdodan <m4a1carbin4@gamil.com>
2022-08-14 18:50:25 +0900

    add Flask simple REST api + KoBERT NLP

---
+++ Python_Flask + NLP/app.py
@@ -0,0 +1,152 @@
+from crypt import methods
+import pandas as pd
+import numpy as np
+from requests import request
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from flask import Flask, request
+from flask_restx import Api, Resource
+import joblib
+import json
+import pickle as pic
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+
+#kobert
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+
+#GPU 사용
+device = torch.device("cuda:0")
+
+#BERT 모델, Vocabulary 불러오기 필수
+bertmodel, vocab = get_pytorch_kobert_model()
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+        
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes = 3, # softmax 사용 <- binary일 경우는 2
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+# Setting parameters
+max_len = 64 # 해당 길이를 초과하는 단어에 대해선 bert가 학습하지 않음
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate = 5e-5
+
+## 학습 모델 로드
+PATH = '/'
+model = torch.load(PATH + 'KoBERT_test.pt')  # 전체 모델을 통째로 불러옴, 클래스 선언 필수
+model.load_state_dict(torch.load(PATH + 'koBERT_model_state_dict.pt'))
+
+#토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+def predict(predict_sentence):
+
+    data = [predict_sentence, '0']
+    dataset_another = [data]
+    dataset_another = pd.DataFrame(dataset_another)
+
+    another_test = BERTDataset(dataset_another.values, 0, 1, tok, max_len, True, False)
+    test_dataloader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=5)
+    
+    model.eval()
+
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_dataloader):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+
+        valid_length= valid_length
+        label = label.long().to(device)
+
+        out = model(token_ids, valid_length, segment_ids)
+
+
+        test_eval=[]
+        for i in out:
+            logits=i
+            logits = logits.detach().cpu().numpy()
+
+            if np.argmax(logits) == 0:
+                test_eval.append("hate")
+            elif np.argmax(logits) == 1:
+                test_eval.append("none")
+            elif np.argmax(logits) == 2:
+                test_eval.append("offensive")
+
+        return test_eval[0]
+
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route('/quest')
+class HelloWorld(Resource):
+    def post(self):
+        content = request.get_json()
+
+        print(content)
+
+        data = pd.read_json(json.dumps(content))
+
+        data = data.txt
+
+        pre_result = predict(data)
+
+        return {">> result of input sentence :  " + pre_result}
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8080)
+
+
